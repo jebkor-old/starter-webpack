0a195e06f69bbc5cda0f55c64baf8136081f1cfc
Jesper Brinch Korsbakke <jeko@insero.com>
2018-01-03 11:16:44 +0100

    moved configs

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,28 +1,30 @@
 ﻿const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const HardSource = require("hard-source-webpack-plugin");
 const baseConfig = require('./webpack.base.config.js');
 
 module.exports = merge(baseConfig, {
-    output: {
-        path: path.resolve(__dirname, '../dist/js/'),
-        filename: '[name].js'
-    },
-    performance: {
-        hints: false
-    },
-    devtool: 'source-map',
-    plugins: [
-        // if imports are found in more than 2 .ts files, they will be moved into a vendor-bundle.js file which needs to be included on the page as well
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.js',
-            minChunks: function (module, count) {
-                if (module.resource && /node_modules/.test(module.resource)) {
-                    return true;
-                }
-                return count >= 2;
-            }
-        })
-    ]
-})
+  output: {
+    path: path.resolve(__dirname, "../dist/js/"),
+    filename: "[name].js"
+  },
+  performance: {
+    hints: false
+  },
+  devtool: "source-map",
+  plugins: [
+    // if imports are found in more than 2 .ts files, they will be moved into a vendor-bundle.js file which needs to be included on the page as well
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.js",
+      minChunks: function(module, count) {
+        if (module.resource && /node_modules/.test(module.resource)) {
+          return true;
+        }
+        return count >= 2;
+      }
+    }),
+    new HardSource()
+  ]
+});
