7d61204ddab0861b57cb8dcadab3495dd2e91143
jebkor <jesper@jebkor.dk>
2018-02-14 14:00:03 +0100

    some changes

--- src/ts/vue/components/App.vue
+++ src/ts/vue/components/App.vue
@@ -45,24 +45,3 @@ export default {
     }
 };
 </script>
-
-
-
-
-
-
-<style lang="scss" scoped>
-#app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif !important;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin-top: 60px;
-
-    img {
-        display: block;
-        margin: 0 auto;
-        width: 200px;
-    }
-}
-</style>
